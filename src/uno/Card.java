4e8ef7fe3c08c06af820df08c470d256f42714eb
jonathan <caojiasen@gmail.com>
2021-12-19 23:13:50 -0800

    convert Card to record

--- src/uno/Card.java
+++ src/uno/Card.java
@@ -3,27 +3,7 @@ package uno;
 /**
  * An Uno card.
  */
-class Card {
-    /**
-     * The color of the card.
-     */
-    private final CardColor color;
-    /**
-     * The type of the card.
-     */
-    private final CardType type;
-
-    /**
-     * Create a new card.
-     *
-     * @param color the color of the card
-     * @param type  the type of the card
-     */
-    Card(CardColor color, CardType type) {
-        this.color = color;
-        this.type = type;
-    }
-
+record Card(CardColor color, CardType type) {
     @Override
     public String toString() {
         return color.toString() + type.toString();
