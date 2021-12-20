9d6135a3efbbc82a3272510e16c97aef26c3d657
jonathan <caojiasen@gmail.com>
2021-12-19 23:23:41 -0800

    not null annotation

--- src/uno/Card.java
+++ src/uno/Card.java
@@ -1,11 +1,14 @@
 package uno;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * An Uno card.
  */
 record Card(CardColor color, CardType type) {
     @Override
-    public String toString() {
+    public @NotNull
+    String toString() {
         return color.toString() + type.toString();
     }
 }
