a0b7d1951dc0e5bbb33ba71a12aedbbfcc2947a9
jonathan <caojiasen@gmail.com>
2021-12-20 19:06:01 -0800

    refactoring, each type tracks frequency and whether the type is wild

--- src/uno/CardType.java
+++ src/uno/CardType.java
@@ -3,50 +3,43 @@ package uno;
 /**
  * Enum for the type of card.
  */
-public enum CardType {
-    ZERO("0"),
-    ONE_0("1"),
-    ONE_1("1"),
-    TWO_0("2"),
-    TWO_1("2"),
-    THREE_0("3"),
-    THREE_1("3"),
-    FOUR_0("4"),
-    FOUR_1("4"),
-    FIVE_0("5"),
-    FIVE_1("5"),
-    SIX_0("6"),
-    SIX_1("6"),
-    SEVEN_0("7"),
-    SEVEN_1("7"),
-    EIGHT_0("8"),
-    EIGHT_1("8"),
-    NINE_0("9"),
-    NINE_1("9"),
-    REVERSE_0("Reverse"),
-    REVERSE_1("Reverse"),
-    SKIP_0("Skip"),
-    SKIP_1("Skip"),
-    DRAW_TWO_0("Draw Two"),
-    DRAW_TWO_1("Draw Two"),
-    WILD_0("Wild"),
-    WILD_1("Wild"),
-    WILD_2("Wild"),
-    WILD_3("Wild"),
-    WILD_DRAW_FOUR_0("Wild Draw Four"),
-    WILD_DRAW_FOUR_1("Wild Draw Four"),
-    WILD_DRAW_FOUR_2("Wild Draw Four"),
-    WILD_DRAW_FOUR_3("Wild Draw Four");
+enum CardType {
+    ZERO("0", 1, false),
+    ONE("1", 2, false),
+    TWO("2", 2, false),
+    THREE("3", 2, false),
+    FOUR("4", 2, false),
+    FIVE("5", 2, false),
+    SIX("6", 2, false),
+    SEVEN("7", 2, false),
+    EIGHT("8", 2, false),
+    NINE("9", 2, false),
+    REVERSE("Reverse", 2, false),
+    SKIP("Skip", 2, false),
+    DRAW_TWO("Draw Two", 2, false),
+    WILD("Wild", 4, true),
+    WILD_DRAW_FOUR("Wild Draw Four", 4, true);
 
     private final String string;
+    private final int freq;
+    private final boolean isWild;
 
-    CardType(String string) {
+    CardType(String string, int freq, boolean isWild) {
         this.string = string;
+        this.freq = freq;
+        this.isWild = isWild;
     }
-
 
     @Override
     public String toString() {
         return string;
+    }
+
+    int getFreq() {
+        return this.freq;
+    }
+
+    boolean isWild() {
+        return this.isWild;
     }
 }
