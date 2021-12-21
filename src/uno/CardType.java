5fc0fb4de08e6cd1b0461886625051f4dda482eb
jonathan <caojiasen@gmail.com>
2021-12-21 10:16:39 -0800

    added card values

--- src/uno/CardType.java
+++ src/uno/CardType.java
@@ -31,7 +31,7 @@ enum CardType {
      * @param string String to print for this card type
      * @param freq   number of cards of this card type in a given color
      * @param isWild whether this card type is wild
-     * @param value  the score value of this card
+     * @param value  the point value of this card type
      */
     CardType(String string, int freq, boolean isWild, int value) {
         this.string = string;
@@ -45,11 +45,24 @@ enum CardType {
         return string;
     }
 
+    /**
+     * @return the number of cards of this card type in a given color
+     */
     int getFreq() {
         return this.freq;
     }
 
+    /**
+     * @return true if this card type is wild, and false otherwise
+     */
     boolean isWild() {
         return this.isWild;
+    }
+
+    /**
+     * @return the point value of this card type
+     */
+    int getValue() {
+        return value;
     }
 }
