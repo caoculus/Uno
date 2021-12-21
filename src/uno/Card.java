8c8fdc6e049ba2d3887f8b2bb5b44d4caa6ff0fb
jonathan <caojiasen@gmail.com>
2021-12-20 19:05:28 -0800

    add compareTo

--- src/uno/Card.java
+++ src/uno/Card.java
@@ -5,10 +5,23 @@ import org.jetbrains.annotations.NotNull;
 /**
  * An Uno card.
  */
-record Card(CardColor color, CardType type) {
+record Card(CardColor color, CardType type, int id)
+    implements Comparable<Card> {
     @Override
-    public @NotNull
-    String toString() {
+    public @NotNull String toString() {
         return color.toString() + type.toString();
+    }
+
+    @Override
+    public int compareTo(@NotNull Card o) {
+        int diff1 = color.compareTo(o.color);
+        if (diff1 != 0) {
+            return diff1;
+        }
+        int diff2 = type.compareTo(o.type);
+        if (diff2 != 0) {
+            return diff2;
+        }
+        return Integer.compare(id, o.id);
     }
 }
