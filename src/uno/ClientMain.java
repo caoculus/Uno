53fb1242ad7981641c702b7980c27332ead648ac
jonathan <caojiasen@gmail.com>
2021-12-25 22:42:51 -0800

    some changes

--- src/uno/ClientMain.java
+++ src/uno/ClientMain.java
@@ -8,7 +8,7 @@ public class ClientMain {
     public static void main(String[] args) throws IOException {
         BufferedReader reader =
             new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter your name: ");
+        System.out.print("Enter your name: ");
         String name = reader.readLine();
         UnoClient client = new UnoClient("localhost", 9000, name);
         client.start();
--- src/uno/ServerMain.java
+++ src/uno/ServerMain.java
@@ -1,8 +1,24 @@
 package uno;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ServerMain {
-    public static void main(String[] args) {
-        UnoServer server = new UnoServer(9000, 4);
-        server.start();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print("Enter number of players: ");
+                String input = reader.readLine();
+                int numPlayers = Integer.parseInt(input);
+                UnoServer server = new UnoServer(9000, numPlayers);
+                server.start();
+                break;
+            } catch (NumberFormatException e) {
+                /* fall through */
+            }
+        }
     }
 }
